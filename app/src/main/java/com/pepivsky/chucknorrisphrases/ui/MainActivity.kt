28a09da1922aa15bc8e309bc4645483dcdae1ad9
pepivsky <ironglas68@gmail.com>
2024-07-19 10:57:32 -0600

    created mainScreen

--- app/src/main/java/com/pepivsky/chucknorrisphrases/ui/MainActivity.kt
+++ app/src/main/java/com/pepivsky/chucknorrisphrases/ui/MainActivity.kt
@@ -7,6 +7,15 @@ import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.ads.AdRequest
 import com.pepivsky.chucknorrisphrases.R
 import com.pepivsky.chucknorrisphrases.core.Resource
@@ -31,7 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            MainScreen()
         }
 
         /*binding = ActivityMainBinding.inflate(layoutInflater)
@@ -69,5 +78,16 @@ class MainActivity : ComponentActivity() {
     private fun initLoadAds() {
         val adRequest = AdRequest.Builder().build()
         binding.banner.loadAd(adRequest)
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun MainScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFFFF7878))) {
+        Text(modifier = Modifier.align(Alignment.TopCenter),text ="Tap Here")
+        Text(modifier = Modifier.align(Alignment.Center),text = "Phrase")
+
     }
 }
