71635fa1ff718848048a71475b6424ce8d660638
José Manuel Paredes Ramírez <josemanuel.paredes@suramexico.com>
2025-12-25 12:44:44 -0600

    updated project

--- app/src/main/java/com/pepivsky/chucknorrisphrases/ui/screens/MainScreen.kt
+++ app/src/main/java/com/pepivsky/chucknorrisphrases/ui/screens/MainScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -58,7 +59,7 @@ fun MainScreen(phraseViewModel: PhraseViewModel) {
                 .weight(1F)
                 .fillMaxWidth()
                 .clickable(
-                    interactionSource = MutableInteractionSource(),
+                    interactionSource = remember { MutableInteractionSource() },
                     indication = rememberRipple(),
                     onClick = {
 
